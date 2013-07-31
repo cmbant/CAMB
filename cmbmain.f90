7926e756e647e8ab9f7120c59a1bf3526238fb08
antony@cosmologist.info <antony@cosmologist.info>
2013-07-31 10:28:54 +0200

    fix gfortran compatibility (again)

--- cmbmain.f90
+++ cmbmain.f90
@@ -2390,7 +2390,8 @@
     implicit none
     Type(ClTransferData) :: CTransS, CTransT, CTransV
     integer in,i,j
-    integer, parameter :: ind(2,2)= (/(/ 1,3 /),(/ 3,2 /)/) ! [[1,3],[3,2]]
+    integer, dimension(2,2), parameter :: ind = reshape( (/ 1,3,3,2 /), shape(ind))
+    !use verbose form above for gfortran consistency  [[1,3],[3,2]]
 
     !Note using log interpolation is worse
 
