a89adde3c5a0cacdc36e6d094ec4a5a03000d380
antony@cosmologist.info <antony@cosmologist.info>
2013-09-13 14:13:19 +0100

    update doc and version

--- modules.f90
+++ modules.f90
@@ -35,7 +35,7 @@
     implicit none
     public
 
-    character(LEN=*), parameter :: version = 'July13'
+    character(LEN=*), parameter :: version = 'Aug13'
 
     integer :: FeedbackLevel = 0 !if >0 print out useful information about the model
 
