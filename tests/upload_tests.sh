47d3c85fc0ab43ae0c8a5d1edcf261d0bee053ce
Antony Lewis <antony@cosmologist>
2015-10-07 17:10:16 +0100

    fix

--- tests/upload_tests.sh
+++ tests/upload_tests.sh
@@ -1,8 +1,7 @@
-mkdir tmp
-cd tmp
 git config user.name "Travis CI"
 git config user.email "antony@cosmologist.info"
 git clone -b ${TRAVIS_BRANCH}_travis --depth 1 "https://$GH_TOKEN@github.com/cmbant/CAMB_test_outputs" > /dev/null 2>&1
+cd CAMB_test_outputs
 git rm -r *
 cp -r ../testfiles/* .
 git add --all .
