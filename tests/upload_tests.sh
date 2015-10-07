38c02f4f3bbe482561681344680b48383f6b9167
Antony Lewis <antony@cosmologist>
2015-10-07 17:47:18 +0100

    fix

--- tests/upload_tests.sh
+++ tests/upload_tests.sh
@@ -1,15 +1,15 @@
-git config user.name "Travis CI"
-git config user.email "antony@cosmologist.info"
 git clone -b ${TRAVIS_BRANCH}_travis --depth 1 "https://$GH_TOKEN@github.com/cmbant/CAMB_test_outputs" > /dev/null 2>&1
 cd CAMB_test_outputs
-git rm -r *
+git config user.name "Travis CI"
+git config user.email "antony@cosmologist.info"
+git rm -r * > /dev/null 2>&1
 cp -r ../testfiles/* .
 git add --all .
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER for ${TRAVIS_BRANCH} commit $TRAVIS_COMMIT"
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER for $TRAVIS_BRANCH commit $TRAVIS_COMMIT"
 git push --quiet origin ${TRAVIS_BRANCH}_travis > /dev/null 2>&1
 
 #git init
 #git add --all .
-#git commit -m "Travis build $TRAVIS_BUILD_NUMBER for ${TRAVIS_BRANCH} commit $TRAVIS_COMMIT"
+#git commit -m "Travis build $TRAVIS_BUILD_NUMBER for $TRAVIS_BRANCH commit $TRAVIS_COMMIT"
 #git push --force --quiet "https://$GH_TOKEN@github.com/cmbant/CAMB_test_outputs" master:${TRAVIS_BRANCH}_travis > /dev/null 2>&1
 
