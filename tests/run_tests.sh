d1d99929e32a9eb2fa680ae8842d5269211067d0
Antony Lewis <antony@cosmologist>
2015-10-07 18:44:11 +0100

    add unit tests and Travis integration

---
+++ tests/run_tests.sh
@@ -0,0 +1,16 @@
+cd /camb/forutils
+make Release
+cd /camb
+make Release
+
+
+python python/CAMB_test_files.py testfiles --make_ini
+cd testfiles
+git clone -b $TRAVIS_BRANCH --depth=1 https://github.com/cmbant/CAMB_test_outputs.git
+cd ..
+
+python python/CAMB_test_files.py testfiles --diff_to CAMB_test_outputs/test_outputs --verbose
+rc=$?
+
+rm -Rf testfiles/CAMB_test_outputs
+exit $rc
