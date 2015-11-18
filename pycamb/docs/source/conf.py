5c80e37f25156fd518e6566558a8a3524d12e0fc
Antony Lewis <antony@cosmologist>
2015-11-18 12:04:39 +0000

    docs

--- pycamb/docs/source/conf.py
+++ pycamb/docs/source/conf.py
@@ -21,7 +21,7 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'..','..')))
+sys.path.insert(0, '../..')
 
 # -- General configuration ------------------------------------------------
 
@@ -204,7 +204,7 @@ html_theme_options = {}
 # html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'GetDistDoc'
+htmlhelp_basename = 'CAMBDoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
