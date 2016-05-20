164cfd73d4f9d3236d90bb9e1aa978eb9e6362cf
Antony Lewis <antony@cosmologist.info>
2016-05-20 12:18:11 +0100

    version bump

--- modules.f90
+++ modules.f90
@@ -35,7 +35,7 @@
     implicit none
     public
 
-    character(LEN=*), parameter :: version = 'Nov15'
+    character(LEN=*), parameter :: version = 'May16'
 
     integer :: FeedbackLevel = 0 !if >0 print out useful information about the model
 
--- pycamb/camb/__init__.py
+++ pycamb/camb/__init__.py
@@ -6,8 +6,8 @@ Python CAMB interface (http://camb.info)
 """
 __author__ = "Antony Lewis"
 __contact__ = "antony at cosmologist dot info"
-__status__ = "alpha"
-__version__ = "0.1.1"
+__status__ = "beta"
+__version__ = "0.1.2"
 
 from .baseconfig import dll_import
 from .camb import CAMBdata, MatterTransferData, get_results, get_transfer_functions, get_background, \
