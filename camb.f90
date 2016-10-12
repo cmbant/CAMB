0a058cc81e68de17d641b0fa8361052444d2ba74
Antony Lewis <antony@cosmologist.info>
2016-10-12 14:58:12 +0100

    Revert "set nu_mass_numbers for mnu<0.06 normal hierarchy"
    This reverts commit a1fb905244460cb7229657d0c035ff1f71d65245.

--- camb.f90
+++ camb.f90
@@ -441,7 +441,6 @@
                 P%Nu_mass_numbers(1) = P%Num_Nu_Massive
                 P%Nu_mass_eigenstates = 1
                 P%Nu_mass_fractions(1) = normal_frac
-                P%Nu_mass_numbers(1) = 1                
             end if
         end if
     else
