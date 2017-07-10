941ff2992eecd516c5cd7621853021e3352ed3ae
Antony Lewis <antony@cosmologist.info>
2017-07-10 16:21:34 +0100

    minor

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -12,9 +12,9 @@ python python/CAMB_test_files.py testfiles --make_ini
 cd testfiles
 case "$TRAVIS_BRANCH" in
  devel) BRANCH="devel" ;;
-    *) SERVER_LOGIN="master" ;;
+    *) BRANCH="master" ;;
 esac
-git clone -b $TRAVIS_BRANCH --depth=1 https://github.com/cmbant/CAMB_test_outputs.git
+git clone -b $BRANCH --depth=1 https://github.com/cmbant/CAMB_test_outputs.git
 cd ..
 
 python python/CAMB_test_files.py testfiles --diff_to CAMB_test_outputs/test_outputs --verbose
