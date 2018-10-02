4a584ca6c82f0dd0f8230f428c80cbf81e8fa651
Antony Lewis <antony@cosmologist.info>
2018-10-02 17:36:44 +0100

    travis fix

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -13,8 +13,8 @@ rm -Rf dist/*
 rm -Rf build/*
 rm -f camb/*.so
 
-if [[ $TRAVIS_REPO_SLUG == "cmbant/CAMB" && test $CHANNEL == "defaults" && "$TRAVIS_PULL_REQUEST" == "false" ]]
-then 
+if [[ $TRAVIS_REPO_SLUG == "cmbant/CAMB" && $CHANNEL == "defaults" && "$TRAVIS_PULL_REQUEST" == "false" ]]
+then
  case "$TRAVIS_BRANCH" in
  devel*) export CAMB_PACKAGE_NAME=camb_devel ;;
     *) export CAMB_PACKAGE_NAME=camb
