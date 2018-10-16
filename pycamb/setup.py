537a95e98b7cee126fe8a4533fa92edad3a6f9c6
Antony Lewis <antony@cosmologist.info>
2018-10-16 10:11:45 +0100

    update forutils

--- pycamb/setup.py
+++ pycamb/setup.py
@@ -233,7 +233,8 @@ if __name__ == "__main__":
               'Programming Language :: Python :: 3',
               'Programming Language :: Python :: 3.4',
               'Programming Language :: Python :: 3.5',
-              'Programming Language :: Python :: 3.6'
+              'Programming Language :: Python :: 3.6',
+              'Programming Language :: Python :: 3.7'
           ],
           keywords=['cosmology', 'CAMB']
           )
