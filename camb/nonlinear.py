0d3d7c9b210e257cbf6e2fcf0af5f9a38725ad0c
tilmantroester <tilman.troester@gmail.com>
2019-07-11 12:41:06 +0100

    Expose HMCode baryon parameters (#46)
    * Expose HMCode baryon parameters.
    
    * Default values for baryon parameters when called directly from Fortran.

--- camb/nonlinear.py
+++ camb/nonlinear.py
@@ -32,7 +32,9 @@ class Halofit(NonLinearModel):
     Various specific approximate non-linear correction models based on HaloFit.
     """
     _fields_ = [
-        ("halofit_version", c_int, {"names": halofit_version_names, "start": 1})
+        ("halofit_version", c_int, {"names": halofit_version_names, "start": 1}),
+        ("HMCode_A_baryon", c_double, "HMCode parameter A_baryon"),
+        ("HMCode_eta_baryon", c_double, "HMCode parameter eta_baryon"),
     ]
 
     _fortran_class_module_ = 'NonLinear'
@@ -41,7 +43,7 @@ class Halofit(NonLinearModel):
     def get_halofit_version(self):
         return self.halofit_version
 
-    def set_params(self, halofit_version=halofit_default):
+    def set_params(self, halofit_version=halofit_default, HMCode_A_baryon=3.13, HMCode_eta_baryon=0.603):
         """
         Set the halofit model for non-linear corrections.
 
@@ -55,9 +57,12 @@ class Halofit(NonLinearModel):
             - halomodel: basic halomodel
             - casarini: PKequal `arXiv:0810.0190 <https://arxiv.org/abs/0810.0190>`_, `arXiv:1601.07230 <https://arxiv.org/abs/1601.07230>`_
             - mead2015: original 2015 version of HMCode `arXiv:1505.07833 <https://arxiv.org/abs/1505.07833>`_
-
+        :param HMCode_A_baryon: HMCode parameter A_baryon. Default 3.13.
+        :param HMCode_eta_baryon: HMCode parameter eta_baryon. Default 0.603.
         """
         self.halofit_version = halofit_version
+        self.HMCode_A_baryon = HMCode_A_baryon
+        self.HMCode_eta_baryon = HMCode_eta_baryon
 
 
 @fortran_class
