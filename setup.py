a70195e3a81a9f6e81b7b9b69cb2a6838cc2b1f4
Antony Lewis <antony@cosmologist.info>
2020-11-12 08:17:46 +0000

    typo

--- setup.py
+++ setup.py
@@ -301,7 +301,7 @@ if __name__ == "__main__":
               'Programming Language :: Python :: 3',
               'Programming Language :: Python :: 3.6',
               'Programming Language :: Python :: 3.7',
-              'Programming Language :: Python :: 3.8'
+              'Programming Language :: Python :: 3.8',
               'Programming Language :: Python :: 3.9'
           ],
           keywords=['cosmology', 'CAMB', 'CMB'],
