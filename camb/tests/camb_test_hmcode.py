0d46acd187ab3f2e8e6015fc02f2b892f9f374a2
Alexander Mead <alexander.j.mead@googlemail.com>
2021-02-05 15:27:28 +0000

    neutrino test improvement; speed increase by factor 10

--- camb/tests/camb_test_hmcode.py
+++ camb/tests/camb_test_hmcode.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import unittest
-#import platform
 import numpy as np
 
 try:
@@ -9,8 +8,6 @@ try:
 except ImportError as e:
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
     import camb
-#from camb import model, correlations, bbn, dark_energy, initialpower
-#from camb.baseconfig import CAMBParamRangeError, CAMBValueError
 
 fast = 'ci fast' in os.getenv("TRAVIS_COMMIT_MESSAGE", "")
  
@@ -19,23 +16,27 @@ class CambTest(unittest.TestCase):
     def testHMcode(self):
 
             # Parameters
+            # TODO: Not clear that the neutrino setup here agrees exactly with my HMx setup
+            # TODO: Larger errors than I would like with high-massive-nu and high-baryon cosmologies
+            # TODO: Normalisation via As not agreeing exactly with sigma_8 values
             kmax_calc = 2e2 # Maximum wavenumber for the CAMB calculation [h/Mpc]
             pi = np.pi # Lovely pi
             twopi = 2.*pi # Twice as lovely pi
-            As_def = 2e-9 # Default As value
-            neutrino_number = 94.14 # Neutrino closure mass [eV] VERY LAZY
+            neutrino_number = 94.14 # Neutrino closure mass [eV] TODO: VERY LAZY (should it be 93.14, 94.07eV?)
             eps_k = 1e-6 # Fractional error tolerated in k
             eps_a = 1e-6 # Fractional error tolerated in a
-            eps_Pk = 5e-3 # Fractional error tolerated in Pk
+            eps_Pk = 5e-3 # Fractional error tolerated in Pk # TODO: This should agree to 1e-3 as in HMx
             kmin_test = 1e-2 # Minimum wavenumber for test [h/Mpc]
             kmax_test = 1e1  # Maximum wavenumber for test [h/Mpc]
             amin_test = 0.333 # Minimum scale factor for test
             amax_test = 1.000 # Maximum scale factor for test
             Y_He = 0.24 # Helium fraction
             T_CMB = 2.725 # CMB temperature
-            num_massive_nu = 3 # Number of massive neutrinos
-            hierarchy = 'degenerate' # Neutrino hierarchy
+            nnu = 3 # Number of massive neutrinos
+            neff = 3.046 # Effective number of radiative neutrinos
             verbose = False # Verbose tests
+            norm_sig8 = False # Normalisation by sigma_8 or As?
+            a_in = np.array([0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]) # Scale factors for tests
 
             # Dictionary for HMcode versions
             HMcode_version_ihm = {
@@ -50,10 +51,10 @@ class CambTest(unittest.TestCase):
             def read_Mead_benchmark(infile):
                 
                 # Read data and split into k, a and Pk
-                results = np.loadtxt(infile)
-                k = results[:, 0]
-                a = np.array([0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
-                D2 = results[:, 1:]       
+                data = np.loadtxt(infile)
+                k = data[:, 0]
+                a = a_in
+                D2 = data[:, 1:]       
                 Pk = (D2.T/(4.*pi*(k/twopi)**3)).T # Convert Delta^2(k) -> P(k)
                 
                 # Return results
@@ -67,6 +68,7 @@ class CambTest(unittest.TestCase):
                 h = 0.7
                 ns = 0.96
                 sig8 = 0.8
+                As = 1.97269e-9
                 w0 = -1.
                 wa = 0.
                 mnu = 0.
@@ -79,40 +81,53 @@ class CambTest(unittest.TestCase):
                     h = 0.6732
                     ns = 0.96605
                     sig8 = 0.8120
+                    As = 2.08924e-9
                     w0 = -1.
                     wa = 0.
                     mnu = 0.06
                 elif (icos == 241):
                     w0 = -0.7
+                    As = 2.46981e-9
                 elif (icos == 242):
                     w0 = -1.3
+                    As = 1.75070e-9
                 elif (icos == 243):
                     mnu = 0.3
+                    As = 2.35868e-9
                 elif (icos == 244):
                     mnu = 0.9
+                    As = 3.43507e-9
                 elif (icos == 245):
                     ns = 0.7
+                    As = 2.38515e-9
                 elif (icos == 246):
                     ns = 1.3
+                    As = 1.47601e-9
                 elif (icos == 247):
                     Om_b = 0.01
+                    As = 1.20028e-9
                 elif (icos == 248):
                     Om_b = 0.1
+                    As = 3.88822e-9
                 elif (icos == 249):
                     wa = 0.9
+                    As = 3.35538e-9
                 elif (icos == 250):
                     logT = 7.6
                 elif (icos == 251):
                     logT = 8.0
                     
                 # Return cosmological parameters
-                return Om_m, Om_b, h, ns, sig8, w0, wa, mnu, logT
+                return Om_m, Om_b, h, ns, sig8, As, w0, wa, mnu, logT
 
             # Set up a CAMB parameter set for a Mead cosmology
-            def setup_HMcode_test(pars, icos):
+            def setup_HMcode_test(a, icos):
                 
                 # Get the cosmological parameters for the cosmology
-                Om_m, Om_b, h, ns, sig8, w0, wa, mnu, logT = HMcode_test_cosmologies(icos)
+                Om_m, Om_b, h, ns, sig8, As, w0, wa, mnu, logT = HMcode_test_cosmologies(icos)
+
+                # Redshifts
+                z = -1.+1./a
 
                 # Derive and set CAMB parameters
                 H0 = 100.*h
@@ -122,25 +137,41 @@ class CambTest(unittest.TestCase):
                 Om_k = 0.
                 wb = Om_b*h**2
                 wc = Om_c*h**2
-                pars.set_cosmology(H0=H0, ombh2=wb, omch2=wc, mnu=mnu, omk=Om_k, YHe=Y_He, TCMB=T_CMB,
-                    num_massive_neutrinos=num_massive_nu, neutrino_hierarchy=hierarchy)
+
+                # Set parameters using the traditional Fortran languge
+                # TODO: Check carefully against my ini files
+                pars = camb.CAMBparams(WantCls=False, H0=H0, ombh2=wb, omch2=wc, omnuh2=wnu, omk=Om_k, YHe=Y_He, TCMB=T_CMB,
+                                        num_nu_massive=nnu,
+                                        num_nu_massless=neff-nnu,
+                                        nu_mass_numbers=[nnu],
+                                        nu_mass_degeneracies=[neff/nnu],
+                                        nu_mass_fractions=[1.],
+                                        share_delta_neff=True,
+                                        )
                 pars.set_dark_energy(w=w0, wa=wa, dark_energy_model='ppf')
-                pars.InitPower.set_params(As=As_def, ns=ns)
+                pars.InitPower.set_params(As=As, ns=ns)
 
-                # Set parameters for matter spectrum calculation
-                # It is wasteful to do this; could get As directly for each test cosmology
-                pars.set_matter_power(redshifts=[0.], kmax=kmax_calc)
-                pars.NonLinear = camb.model.NonLinear_none
+                # Set sigma_8 normalisation (wasteful)
+                if (norm_sig8):
+                    pars.set_matter_power(redshifts=[0.], kmax=kmax_calc)
+                    pars.NonLinear = camb.model.NonLinear_none
+                    results = camb.get_results(pars)
+                    sig8_init = results.get_sigma8()
+                    As = As*(sig8/sig8_init)**2
+                    pars.InitPower.set_params(As=As, ns=ns) # Reset As
+
+                # Main calculation step
+                pars.set_matter_power(redshifts=z, kmax=kmax_calc)
+                pars.NonLinear = camb.model.NonLinear_both
                 results = camb.get_results(pars)
-                sig8_init = results.get_sigma8()
-                As = As_def*(sig8/sig8_init)**2
-                pars.InitPower.set_params(As=As, ns=ns) # Reset As
+                if (verbose): 
+                    print(pars)
 
-                # Return AGN temperature (ugly, but it needs to come out)
-                return logT
+                # Return results and AGN temperature (ugly, but it needs to come out)
+                return results, logT
 
             # Get the HMcode power from CAMB
-            def get_HMcode_power_from_CAMB(pars, k_in, a_in, logT, HMcode_version):
+            def get_HMcode_power_from_CAMB(results, k_in, a_in, logT, HMcode_version):
 
                 # k and z ranges for results
                 kmin = k_in[0]
@@ -149,15 +180,13 @@ class CambTest(unittest.TestCase):
                 z = -1.+1./a_in
 
                 # Get non-linear spectra
-                pars.set_matter_power(redshifts=z, kmax=kmax_calc)
-                pars.NonLinear = camb.model.NonLinear_both
-                results = camb.get_results(pars)
-                results.calc_power_spectra(pars)
                 results.Params.NonLinearModel.set_params(halofit_version=HMcode_version, HMCode_logT_AGN=logT)
                 k, z, Pk = results.get_matter_power_spectrum(minkh=kmin, maxkh=kmax, npoints=nk)
                 Pk = Pk.T[:, ::-1]
                 z = np.array(z)[::-1]      
                 a = 1./(1.+z)
+                if (verbose):
+                    print('sigma_8:', results.get_sigma8()[-1])
                 
                 # Return non-linear power
                 return k, a, Pk
@@ -170,11 +199,13 @@ class CambTest(unittest.TestCase):
             if(verbose): print('')
 
             # Loop over cosmologies
-            #for icos in [26, 56, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251]:
-            for icos in [26, 56]:
+            for icos in [26, 56, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251]:
+
+                # Init CAMB
+                results, logT = setup_HMcode_test(a_in, icos)
 
                 # Loop over HMcode versions
-                for HMcode_version in ['mead2015', 'mead2016', 'mead2020', 'mead2020_feedback']:    
+                for HMcode_version in ['mead2015', 'mead2016', 'mead2020', 'mead2020_feedback']:  
 
                     # Read benchmark data
                     ihm = HMcode_version_ihm[HMcode_version]
@@ -182,10 +213,8 @@ class CambTest(unittest.TestCase):
                     if(verbose): print('Infile:', infile)
                     k_in, a_in, Pk_in = read_Mead_benchmark(infile)
 
-                    # Get power from CAMB
-                    params = camb.CAMBparams(WantCls=False)
-                    logT = setup_HMcode_test(params, icos)
-                    k_nl, a_nl, Pk_nl = get_HMcode_power_from_CAMB(params, k_in, a_in, logT, HMcode_version)
+                    # Get power from CAMB                   
+                    k_nl, a_nl, Pk_nl = get_HMcode_power_from_CAMB(results, k_in, a_in, logT, HMcode_version)
 
                     # Compare benchmark to calculation
                     for ik in range(len(k_in)):
