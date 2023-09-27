fd8874b7529436422225419372eb58765da6e47c
Antony Lewis <antony@cosmologist.info>
2023-09-27 13:37:42 +0100

    python 12

--- setup.py
+++ setup.py
@@ -306,7 +306,8 @@ if __name__ == "__main__":
               'Programming Language :: Python :: 3.8',
               'Programming Language :: Python :: 3.9',
               'Programming Language :: Python :: 3.10',
-              'Programming Language :: Python :: 3.11'
+              'Programming Language :: Python :: 3.11',
+              'Programming Language :: Python :: 3.12'
           ],
           keywords=['cosmology', 'CAMB', 'CMB'],
           install_requires=['scipy>=1.0', 'sympy>=1.0', 'packaging'],
