d3e2e2bb44e895dd7e9c4f2b2be21e6dad36a3dc
Antony Lewis <antony@cosmologist.info>
2024-06-06 10:37:21 +0100

    update [pypi]

--- setup.py
+++ setup.py
@@ -279,7 +279,6 @@ def find_version():
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         version = version_match.group(1)
-        print("Run number %s" % os.getenv('GITHUB_RUN_NUMBER'))
         commit = os.getenv('GITHUB_RUN_NUMBER')
         if commit and not os.getenv('GITHUB_REF', '').startswith('refs/tags/'):
             version += '.' + commit
