ce1c84f9bbadd776333d72bef8bc6165dc1c7ac7
Antony Lewis <antony@cosmologist.info>
2025-08-25 21:07:03 +0100

    bump version 1.63

--- fortran/config.f90
+++ fortran/config.f90
@@ -3,7 +3,7 @@
     use constants, only: const_twopi
     implicit none
 
-    character(LEN=*), parameter :: version = '1.6.2'
+    character(LEN=*), parameter :: version = '1.6.3'
 
     integer :: FeedbackLevel = 0 !if >0 print out useful information about the model
 
